2279d4f72ac24c23607fee6628d9aa8be7fccc2c
AbhiNaik988 <abhinaik988@gmail.com>
2023-11-26 08:38:00 +0530

    initital project

---
+++ src/main/java/com/abhinaik/crud_amigoscode/CrudAmigoscodeApplication.java
@@ -0,0 +1,13 @@
+package com.abhinaik.crud_amigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAmigoscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAmigoscodeApplication.class, args);
+	}
+
+}
